5764e3c3ff8d0f32a9b20d8ad801322628cc3c5d
Tim Geurts <geurts.tim@gmail.com>
2016-04-18 14:48:36 +0100

    configure express

---
+++ index.js
@@ -0,0 +1,64 @@
+//index.js/
+import express from 'express';
+import exphbs from 'express-handlebars';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import TwitterStrategy from 'passport-twitter';
+import GoogleStrategy from 'passport-google';
+import FacebookStrategy from 'passport-facebook';
+
+const app = express();
+
+//===============PASSPORT===============
+
+//This section will contain our work with Passport
+
+//===============EXPRESS================
+// Configure Express
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  const err = req.session.error;
+  const msg = req.session.notice;
+  const success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
+// Configure express to use handlebars templates
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//===============ROUTES===============
+
+//This section will hold our Routes
+
+//===============PORT=================
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log("listening on " + port + "!");
