63d2035e38d21c9f7ebfc6c480869490fb204002
Tim Geurts <geurts.tim@gmail.com>
2016-04-19 21:06:58 +0100

    add CRUD for recipes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 import express from 'express';
+import mongoose from 'mongoose';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
@@ -11,6 +12,12 @@ import TwitterStrategy from 'passport-twitter';
 import GoogleStrategy from 'passport-google';
 import FacebookStrategy from 'passport-facebook';
 
+// Models
+import Recipe from './src/models/Recipe';
+
+// Connect to the MongoDB
+mongoose.connect('mongodb://localhost:27017/recipe-book');
+
 const app = express();
 
 //===============PASSPORT===============
@@ -21,7 +28,7 @@ const app = express();
 // Configure Express
 app.use(logger('combined'));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
@@ -54,6 +61,86 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
   res.json({ message: 'It works!' });
+});
+
+//
+const recipesRoute = router.route('/recipes');
+
+// Create endpoint /api/v1/recipes for GET
+recipesRoute.get(function(req, res) {
+  Recipe.find(function(err, recipes) {
+    if (err) res.send(err);
+
+    res.json(recipes);
+  });
+});
+
+// Create endpoint /api/v1/recipes for POST
+recipesRoute.post(function(req, res) {
+  const recipe = new Recipe();
+  const d = req.body;
+
+  // Set the recipe properties that came from the POST data
+  recipe.title = d.title;
+  recipe.author = d.author;
+  recipe.imageurl = d.imageurl;
+  recipe.faves = d.faves;
+  recipe.method = d.method;
+  recipe.ingredients = d.ingredients;
+  recipe.updated = d.updated;
+  recipe.isPublished = d.isPublished;
+
+  recipe.save(function(err) {
+    if (err) res.send(err);
+
+    res.json({ message: 'Recipe added!', data: recipe });
+  });
+});
+
+// Create a new route with the /recipes/:recipe_id prefix
+var recipeRoute = router.route('/recipes/:recipe_id');
+
+// Create endpoint /api/v1/recipes/:recipe_id for GET
+recipeRoute.get(function(req, res) {
+  Recipe.findById(req.params.recipe_id, function(err, recipe) {
+    if (err) res.send(err);
+
+    res.json(recipe);
+  });
+});
+
+// Create endpoint /api/v1/recipes/:recipe_id for PUT
+recipeRoute.put(function(req, res) {
+  const d = req.body;
+
+  Recipe.findById(req.params.recipe_id, function(err, recipe) {
+    if (err) res.send(err);
+
+    // Update the recipe properties that came from the PUT data
+    if (d.title) recipe.title = d.title;
+    if (d.author) recipe.author = d.author;
+    if (d.imageurl) recipe.imageurl = d.imageurl;
+    if (d.faves) recipe.faves = d.faves;
+    if (d.method) recipe.method = d.method;
+    if (d.ingredients) recipe.ingredients = d.ingredients;
+    if (d.updated) recipe.updated = d.updated;
+    if (d.isPublished) recipe.isPublished = d.isPublished;
+
+    recipe.save(function(err) {
+      if (err) res.send(err);
+
+      res.json(recipe);
+    });
+  });
+});
+
+// Create endpoint /api/v1/recipes/:recipe_id for DELETE
+recipeRoute.delete(function(req, res) {
+  Recipe.findByIdAndRemove(req.params.recipe_id, function(err) {
+    if (err) res.send(err);
+
+    res.json({ message: 'Recipe deleted' });
+  });
 });
 
 // REGISTER OUR ROUTES
