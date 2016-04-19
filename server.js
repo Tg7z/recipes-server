bcf10acdc6061aff335c771ad0219124c411bcb4
Tim Geurts <geurts.tim@gmail.com>
2016-04-19 21:27:05 +0100

    clean up via recipes controller

--- server.js
+++ server.js
@@ -12,17 +12,23 @@ import TwitterStrategy from 'passport-twitter';
 import GoogleStrategy from 'passport-google';
 import FacebookStrategy from 'passport-facebook';
 
-// Models
-import Recipe from './src/models/Recipe';
+// Controllers
+import * as recipeController from './src/controllers/recipe';
 
 // Connect to the MongoDB
 mongoose.connect('mongodb://localhost:27017/recipe-book');
 
 const app = express();
 
+
+
+
 //===============PASSPORT===============
 
 //This section will contain our work with Passport
+
+
+
 
 //===============EXPRESS================
 // Configure Express
@@ -52,100 +58,30 @@ app.use(function(req, res, next){
   next();
 });
 
-// configure app to use bodyParser()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+
 
 //===============ROUTES===============
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.json({ message: 'It works!' });
-});
+// Create endpoint handlers for /recipes
+router.route('/recipes')
+  .get(recipeController.getRecipes)
+  .post(recipeController.postRecipes);
 
-//
-const recipesRoute = router.route('/recipes');
+// Create endpoint handlers for /recipes/:recipe_id
+router.route('/recipes/:recipe_id')
+  .get(recipeController.getRecipe)
+  .put(recipeController.putRecipe)
+  .delete(recipeController.deleteRecipe);
 
-// Create endpoint /api/v1/recipes for GET
-recipesRoute.get(function(req, res) {
-  Recipe.find(function(err, recipes) {
-    if (err) res.send(err);
-
-    res.json(recipes);
-  });
-});
-
-// Create endpoint /api/v1/recipes for POST
-recipesRoute.post(function(req, res) {
-  const recipe = new Recipe();
-  const d = req.body;
-
-  // Set the recipe properties that came from the POST data
-  recipe.title = d.title;
-  recipe.author = d.author;
-  recipe.imageurl = d.imageurl;
-  recipe.faves = d.faves;
-  recipe.method = d.method;
-  recipe.ingredients = d.ingredients;
-  recipe.updated = d.updated;
-  recipe.isPublished = d.isPublished;
-
-  recipe.save(function(err) {
-    if (err) res.send(err);
-
-    res.json({ message: 'Recipe added!', data: recipe });
-  });
-});
-
-// Create a new route with the /recipes/:recipe_id prefix
-var recipeRoute = router.route('/recipes/:recipe_id');
-
-// Create endpoint /api/v1/recipes/:recipe_id for GET
-recipeRoute.get(function(req, res) {
-  Recipe.findById(req.params.recipe_id, function(err, recipe) {
-    if (err) res.send(err);
-
-    res.json(recipe);
-  });
-});
-
-// Create endpoint /api/v1/recipes/:recipe_id for PUT
-recipeRoute.put(function(req, res) {
-  const d = req.body;
-
-  Recipe.findById(req.params.recipe_id, function(err, recipe) {
-    if (err) res.send(err);
-
-    // Update the recipe properties that came from the PUT data
-    if (d.title) recipe.title = d.title;
-    if (d.author) recipe.author = d.author;
-    if (d.imageurl) recipe.imageurl = d.imageurl;
-    if (d.faves) recipe.faves = d.faves;
-    if (d.method) recipe.method = d.method;
-    if (d.ingredients) recipe.ingredients = d.ingredients;
-    if (d.updated) recipe.updated = d.updated;
-    if (d.isPublished) recipe.isPublished = d.isPublished;
-
-    recipe.save(function(err) {
-      if (err) res.send(err);
-
-      res.json(recipe);
-    });
-  });
-});
-
-// Create endpoint /api/v1/recipes/:recipe_id for DELETE
-recipeRoute.delete(function(req, res) {
-  Recipe.findByIdAndRemove(req.params.recipe_id, function(err) {
-    if (err) res.send(err);
-
-    res.json({ message: 'Recipe deleted' });
-  });
-});
 
 // REGISTER OUR ROUTES
 // all of our routes will be prefixed with /api/${version}
 app.use('/api/v1', router);
+
+
+
 
 //===============PORT=================
 const port = process.env.PORT || 5000;
---
+++ src/controllers/recipe.js
@@ -0,0 +1,76 @@
+'use strict';
+import Recipe from '../models/recipe';
+
+// Create endpoint /api/v1/recipes for GET
+exports.getRecipes = function(req, res) {
+  Recipe.find(function(err, recipes) {
+    if (err) res.send(err);
+
+    res.json(recipes);
+  });
+};
+
+// Create endpoint /api/v1/recipes for POST
+exports.postRecipes = function(req, res) {
+  const recipe = new Recipe();
+  const d = req.body;
+
+  // Set the recipe properties that came from the POST data
+  recipe.title = d.title;
+  recipe.author = d.author;
+  recipe.imageurl = d.imageurl;
+  recipe.faves = d.faves;
+  recipe.method = d.method;
+  recipe.ingredients = d.ingredients;
+  recipe.updated = d.updated;
+  recipe.isPublished = d.isPublished;
+
+  recipe.save(function(err) {
+    if (err) res.send(err);
+
+    res.json({ message: 'Recipe added!', data: recipe });
+  });
+};
+
+// Create endpoint /api/v1/recipes/:recipe_id for GET
+exports.getRecipe = function(req, res) {
+  Recipe.findById(req.params.recipe_id, function(err, recipe) {
+    if (err) res.send(err);
+
+    res.json(recipe);
+  });
+};
+
+// Create endpoint /api/v1/recipes/:recipe_id for PUT
+exports.putRecipe = function(req, res) {
+  const d = req.body;
+
+  Recipe.findById(req.params.recipe_id, function(err, recipe) {
+    if (err) res.send(err);
+
+    // Update the recipe properties that came from the PUT data
+    if (d.title) recipe.title = d.title;
+    if (d.author) recipe.author = d.author;
+    if (d.imageurl) recipe.imageurl = d.imageurl;
+    if (d.faves) recipe.faves = d.faves;
+    if (d.method) recipe.method = d.method;
+    if (d.ingredients) recipe.ingredients = d.ingredients;
+    if (d.updated) recipe.updated = d.updated;
+    if (d.isPublished) recipe.isPublished = d.isPublished;
+
+    recipe.save(function(err) {
+      if (err) res.send(err);
+
+      res.json(recipe);
+    });
+  });
+};
+
+// Create endpoint /api/v1/recipes/:recipe_id for DELETE
+exports.deleteRecipe = function(req, res) {
+  Recipe.findByIdAndRemove(req.params.recipe_id, function(err) {
+    if (err) res.send(err);
+
+    res.json({ message: 'Recipe deleted' });
+  });
+};
