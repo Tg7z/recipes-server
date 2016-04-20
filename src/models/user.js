fef81b16d76fe17ce7bfa7c4945ec0a892384827
Tim Geurts <geurts.tim@gmail.com>
2016-04-20 11:51:52 +0100

    add user schema and bcrypt package for encryption

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.pre('save', (callback) => {
+  const user = this;
+
+  // Break out if the password hasn't changed
+  if (!user.isModified('password')) return callback();
+
+  // Password changed so we need to hash it
+  bcrypt.genSalt(5, (err, salt) => {
+    if (err) return callback(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return callback(err);
+      user.password = hash;
+      callback();
+    });
+  });
+});
+
+
+
+module.exports = mongoose.model('User', UserSchema);
