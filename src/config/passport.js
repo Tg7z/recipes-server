15648cac2339081997faac9fa8bbb1c5aa785ff9
Tim Geurts <geurts.tim@gmail.com>
2016-04-23 16:40:57 +0100

    set auth header name as variable

--- src/config/passport.js
+++ src/config/passport.js
@@ -4,10 +4,11 @@ import User from '../models/user';
 import config from '../config/database';
 
 module.exports = function(passport) {
+  const AUTH_HEADER = 'jwt_token';
   const opts = {};
 
   opts.secretOrKey = config.secret;
-  opts.jwtFromRequest = JWT.ExtractJwt.fromHeader('jwt_token');
+  opts.jwtFromRequest = JWT.ExtractJwt.fromHeader(AUTH_HEADER);
 
   passport.use(new JWT.Strategy(opts, function(jwt_payload, callback) {
     User.findOne({id: jwt_payload.id}, function(err, user) {
