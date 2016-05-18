800a7477aec03340021561a9219c65e07a3b0983
Tim Geurts <geurts.tim@gmail.com>
2016-05-18 09:39:21 +0100

    add custom middleware to whitelist domains

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ passportConfig(passport);
 
 //===============EXPRESS================
 // Configure Express
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://127.0.0.1:8000', 'http://localhost:8000'];
+  var origin = req.headers.origin;
+
+  if ( allowedOrigins.indexOf(origin) > -1 ) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  // res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+
+  return next();
+});
+
 app.use(logger('combined'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
